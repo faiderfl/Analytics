4ffd454ac948b361ad03e8559449cdcb7c9ef958
Faider Florez Valencia <faiderfl@outlook.com>
2017-10-12 14:33:17 -0500

    Add files via upload

--- SQL database/DBScriptConsolidated.sql
+++ SQL database/DBScriptConsolidated.sql
@@ -12,7 +12,7 @@ CREATE TABLE [dbo].[SalesConsolidated](
 	[Customer] [varchar](50) NULL,	
 	[TotalProducts] [int] NULL,	
 	[TotalPrice] [int] NULL,
-	[Date] [datetime] NULL,
+	[ConsolidatedDate] [datetime] NULL,
 )
 
 GO
