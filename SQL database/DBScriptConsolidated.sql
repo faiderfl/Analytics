604a4b7e126a3f75f70ecd5c0c72ebed2749d49c
Faider Florez Valencia <faiderfl@outlook.com>
2017-10-12 14:28:21 -0500

    Add files via upload

---
+++ SQL database/DBScriptConsolidated.sql
@@ -0,0 +1,20 @@
+USE [wsbdDatabase]
+GO
+
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[SalesConsolidated](
+	[Customer] [varchar](50) NULL,	
+	[TotalProducts] [int] NULL,	
+	[TotalPrice] [int] NULL,
+	[Date] [datetime] NULL,
+)
+
+GO
+
+
